5747525ce965a37cc8333e1351f1c5da98a55075
CDanh0301 <caoducanh1234@gmail.com>
2025-07-20 16:56:03 +0700

    update with 6 characters long password check, and show and unshow password button for login and sign up

--- src/main/java/fa/project/onlinemovieweb/repo/UserRepo.java
+++ src/main/java/fa/project/onlinemovieweb/repo/UserRepo.java
@@ -1,10 +1,14 @@
 package fa.project.onlinemovieweb.repo;
 
 import fa.project.onlinemovieweb.entities.User;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 public interface UserRepo extends JpaRepository<User,Long> {
     User findByEmail(String email);
     User findByUsername(String username);
+    List<User> findAllByUsernameIgnoreCase(String username);
 }
