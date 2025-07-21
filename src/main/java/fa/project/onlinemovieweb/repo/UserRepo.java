073a66fb3cb6290251aae30517ef492f268a36f2
CDanh0301 <caoducanh1234@gmail.com>
2025-07-21 09:33:52 +0700

    delete spare import

--- src/main/java/fa/project/onlinemovieweb/repo/UserRepo.java
+++ src/main/java/fa/project/onlinemovieweb/repo/UserRepo.java
@@ -8,8 +8,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
 public interface UserRepo extends JpaRepository<User,Long> {
     User findByEmail(String email);
     List<User> findAllByUsernameIgnoreCase(String username);
