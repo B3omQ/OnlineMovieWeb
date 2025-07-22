ab3fb0ce16e860f999b6be7df30e1c856923a46d
longnb085 <longnbhe186897@fpt.edu.vn>
2025-07-22 18:38:51 +0700

    Update(user): add favorite
    - Update MediaProfileController
    - Update media_profile page
    - Update Favortie Repo

--- src/main/java/fa/project/onlinemovieweb/repo/FavoriteRepo.java
+++ src/main/java/fa/project/onlinemovieweb/repo/FavoriteRepo.java
@@ -1,12 +1,16 @@
 package fa.project.onlinemovieweb.repo;
 
 import fa.project.onlinemovieweb.entities.Favorite;
+import fa.project.onlinemovieweb.entities.Media;
 import fa.project.onlinemovieweb.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FavoriteRepo extends JpaRepository<Favorite, Long> {
 
     List<Favorite> findByUser(User user);
+
+    Optional<Favorite> findByUserAndMedia(User user, Media media);
 }
