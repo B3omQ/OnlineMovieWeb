947d46daaa6a0391a3ab7ace808a0e1ab3701ba3
chip71 <vuquang07012004@gmail.com>
2025-07-23 12:31:55 +0700

    update pagination

--- src/main/java/fa/project/onlinemovieweb/controller/GenreController.java
+++ src/main/java/fa/project/onlinemovieweb/controller/GenreController.java
@@ -6,10 +6,15 @@ import fa.project.onlinemovieweb.repo.GenreRepo;
 import fa.project.onlinemovieweb.repo.MediaRepo;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Comparator;
 import java.util.List;
@@ -33,18 +38,32 @@ public class GenreController {
     }
 
     @GetMapping("/genres/{id}")
-    public String showGenre(@PathVariable("id") Long genreId, HttpSession session, Model model) {
+    public String showGenre(@PathVariable("id") Long genreId,
+                            @RequestParam(defaultValue = "1") int page,
+                            HttpSession session, Model model) {
 
         Genre genre = genreRepo.findById(genreId).orElse(null);
-        List<Media> mediaList = mediaRepo.findByGenresContaining(genre);
+        if (genre == null) {
+            return "redirect:/"; // or show an error page
+        }
+
+        int pageSize = 10;
+        Pageable pageable = PageRequest.of(page - 1, pageSize, Sort.by("releaseYear").descending());
+        Page<Media> mediaPage = mediaRepo.findByGenresContaining(genre, pageable);
+
         model.addAttribute("genre", genre);
-        model.addAttribute("allMedia", mediaList);
-        Object user = session.getAttribute("user");
-        model.addAttribute("user", user);
+        model.addAttribute("allMedia", mediaPage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", mediaPage.getTotalPages());
         model.addAttribute("pageTitle", genre.getName());
         model.addAttribute("sectionTitle", genre.getName());
+
+        Object user = session.getAttribute("user");
+        model.addAttribute("user", user);
+
         return "seperated_film";
     }
+
 
 
 }
